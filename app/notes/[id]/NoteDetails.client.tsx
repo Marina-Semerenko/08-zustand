62a04b1f3fc3b41b1591858ecf1bbff96d7736ad
Marina <marina.miram@gmail.com>
2026-05-06 23:26:28 +0300

    .

--- app/notes/[id]/NoteDetails.client.tsx
+++ app/notes/[id]/NoteDetails.client.tsx
@@ -3,9 +3,11 @@
 import { useQuery } from '@tanstack/react-query';
 import { fetchNoteById } from '@/lib/api';
 import css from './NoteDetails.module.css';
+import { useParams } from "next/navigation";
 
-export default function NoteDetailsClient ({ id }: {id: string}){
-    
+export default function NoteDetailsClient () {
+    const params = useParams();
+    const id = params.id as string;
 
     const { data: note, isLoading, error } = useQuery({
         queryKey: ['note', id ],
